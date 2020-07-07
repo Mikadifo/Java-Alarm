cb7d3f86420693686d4528f3bc277c855ad53eae
Mikadifo <michaelpadillaheredia@hotmail.com>
2020-07-07 10:13:31 -0500

    Paquetes y main creados

---
+++ src/com/missclick/app/Main.java
@@ -0,0 +1,13 @@
+package com.missclick.app;
+
+/**
+ *
+ * @author missclickTeam
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        //EL INICIO DE TODO
+    }
+    
+}
