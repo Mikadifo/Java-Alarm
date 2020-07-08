28b033f750aada440852052b002a22ccfce727a6
Mikadifo <michaelpadillaheredia@hotmail.com>
2020-07-08 16:25:30 -0500

    Constructores FILEMANAGER creados

--- src/com/missclick/file/FileManager.java
+++ src/com/missclick/file/FileManager.java
@@ -1,9 +1,11 @@
 package com.missclick.file;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
 
@@ -13,13 +15,27 @@ import java.util.List;
  */
 public class FileManager {
     
+    private final Charset UTF8 = StandardCharsets.UTF_8;
     private Path filePath;
     private List<String> fileLines;
+    
+    public FileManager(Path filePath) {
+        this.filePath = filePath;
+    }
+    
+    public FileManager(String first, String... more) {
+        this.filePath = Paths.get(first, more);
+    }
+    
+    public FileManager(String fileName) {
+        this.filePath = Paths.get(fileName);
+    }
     
     //METODO
     
     private void writeFile(List<String> lines) throws IOException {
-        fileLines = Files.readAllLines(filePath, cs);
-        Files.write(filePath, lines, StandardCharsets.UT, StandardOpenOption.CREATE);
+        readFile();
+        fileLines.addAll(lines);
+        Files.write(filePath, fileLines, UTF8, StandardOpenOption.CREATE);
     }
 }
