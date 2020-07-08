3a41d2f5678164d726a5359ec159a86da5c519d2
JhorusF <JhorusF@DESKTOP-UPUBOMH>
2020-07-08 17:36:17 -0500

    Se creo metodos setAlarm y getAlarm

--- src/com/missclick/file/FileManager.java
+++ src/com/missclick/file/FileManager.java
@@ -1,5 +1,6 @@
 package com.missclick.file;
 
+import com.missclick.alarm.Alarm;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -8,6 +9,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.List;
+import static java.util.stream.Collectors.toList;
 
 /**
  *
@@ -16,7 +18,7 @@ import java.util.List;
 public class FileManager {
     
     private final Charset UTF_8 = StandardCharsets.UTF_8;
-    private Path filePath;
+    private final Path filePath;
     
     public FileManager(Path filePath) {
         this.filePath = filePath;
@@ -33,5 +35,21 @@ public class FileManager {
     private void writeFile(List<String> lines) throws IOException {
         Files.write(filePath, lines, UTF_8, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
     }
-
+    private Alarm setAlarm(String alarmLine[]) {
+        return new Alarm(
+                Integer.valueOf(alarmLine[0]),
+                Integer.valueOf(alarmLine[1]), 
+                Integer.valueOf(alarmLine[2]), 
+                Integer.valueOf(alarmLine[3]), 
+                alarmLine[4], 
+                Integer.valueOf(alarmLine[5]), 
+                alarmLine[6].equals("on")
+        ); 
+  
+    }
+    private List<Alarm> getAlarms() throws IOException{
+       return Files.lines(filePath, UTF_8).map(x ->x.split(";")).map(this::setAlarm).collect(toList());
+       
+    }   
+    
 }
