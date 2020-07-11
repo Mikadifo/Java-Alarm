57d41edc3067c19b7335b5b20b8a3d0cbf97dafe
Michael <michaelpadillaheredia@hotmail.com>
2020-07-11 17:34:59 -0500

    Empezando clase de comborbacion de alarmas(constuctores y metodo start)

--- src/com/mikadifo/threads/Alarms.java
+++ src/com/mikadifo/threads/Alarms.java
@@ -4,6 +4,25 @@ package com.mikadifo.threads;
  *
  * @author missclickTeam
  */
-public class Alarms {
+public class Alarms implements Runnable {
+    
+    private final Thread alarmsThread;
+    
+    public Alarms(String name) {
+        this.alarmsThread = new Thread(this, name);
+    }
+    
+    public Alarms() {
+        this.alarmsThread = new Thread(this);
+    }
+    
+    public void start() {
+        this.alarmsThread.start();
+    }
+
+    @Override
+    public void run() {
+        
+    }
     
 }
