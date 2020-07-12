dad019230650951c2ad7a830cae3eae21c9628b2
Usuario <Usuario@192.168.1.125>
2020-07-11 22:57:47 -0500

    Metodos para controlar el tiempo

--- src/com/mikadifo/threads/UseTime.java
+++ src/com/mikadifo/threads/UseTime.java
@@ -1,9 +1,54 @@
 package com.mikadifo.threads;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.util.Date;
+
 /**
  *
  * @author missclickTeam
  */
-public class UseTime {
+public class UseTime extends Thread {
+
     //Hola
+    Thread timer = new Thread();
+    LocalDate dateOfBirth;
+    LocalDateTime now;
+    LocalDateTime finishTime;
+    Period age;
+
+    public UseTime(LocalDate dateOfBirth) {
+        this.dateOfBirth=dateOfBirth;
+        this.age = Period.between(dateOfBirth, LocalDate.now());
+    }
+    
+
+    @Override
+    public void run() {
+
+        if (age.getYears() >= 18) {
+
+            finishTime = now.plusHours(1);
+            for (;;) {
+                if (now.compareTo(finishTime) == 0) {
+                    System.exit(0);
+                }
+            }
+
+        } else {
+
+            finishTime = now.plusSeconds(4);
+            for (;;) {
+                now = LocalDateTime.now();
+
+                if (now.compareTo(finishTime) == 1) {
+
+                    System.exit(0);
+                }
+
+            }
+        }
+
+    }
 }
