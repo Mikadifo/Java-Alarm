3a34a989422d04fdf7dfd9410c271925b5967a58
Fernanda <Fernanda@DESKTOP-AD7CHGC>
2020-07-15 17:58:50 -0500

    //Interface

--- src/com/missclick/alarm/JFCalendario.java
+++ src/com/missclick/alarm/JFCalendario.java
@@ -62,7 +62,7 @@ public class JFCalendario extends javax.swing.JFrame {
             }
         });
 
-        jCalendar.setForeground(new java.awt.Color(255, 255, 255));
+        jCalendar.setForeground(new java.awt.Color(0, 0, 0));
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
